3934d6088bb7bb3187df2625003ba8d8fcc48e6c
davcitra <davia.ciccoli@ecal.ch>
2025-12-04 11:58:31 +0100

    svg 0

--- internal/main.js
+++ internal/main.js
@@ -14,12 +14,12 @@ try {
 if (sequenceData) {
   sequenceData.forEach((s) => {
     let nextNum = Number.parseInt(s.content) - 1;
-    if (nextNum < 0) nextNum = 3;
+    if (nextNum < 0) nextNum = 0;
     s.nextContent = nextNum.toString();
   });
 
   //console.log(sequenceData);
-  const runner = new SequenceRunner(sequenceData, "3");
+  const runner = new SequenceRunner(sequenceData, "0");
 
   initMenu(runner);
   initTransitionUI(runner);
